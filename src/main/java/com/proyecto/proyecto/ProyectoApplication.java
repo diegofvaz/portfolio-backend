3199ad270395288b0fb6a92ae0d07ed1c483173d
Martu91 <ma.tarantino@hotmail.com>
2022-05-25 17:41:09 +1200

    cambio a la url del fiebase en el @bean

--- src/main/java/com/proyecto/proyecto/ProyectoApplication.java
+++ src/main/java/com/proyecto/proyecto/ProyectoApplication.java
@@ -19,7 +19,7 @@ public class ProyectoApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*")); 
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://proyecto-dv-80d35.web.app")); 
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
